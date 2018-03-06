fd5e35c34ebdcb434da08844b6d9456f94c301fd
Semjon Kerner <semjon.kerner@fu-berlin.de>
2018-03-06 16:16:00 +0100

    initial implementation

---
+++ ampiseq/main.c
@@ -0,0 +1,15 @@
+/* author: Semjon Kerner */
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <math.h>
+
+#include <alsa/asoundlib.h>
+
+#define DEBUG_ENABLE (1)
+#include "include/ampiseq.h"
+
+int main(void) {
+    return 0;
+}
