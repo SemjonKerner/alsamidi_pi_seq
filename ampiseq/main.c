8b2744ec8bf0a996be189d8b06264d672267159a
Semjon Kerner <semjon.kerner@fu-berlin.de>
2018-03-09 18:16:40 +0100

    WIP: links to gpio

--- ampiseq/main.c
+++ ampiseq/main.c
@@ -6,9 +6,15 @@
 #include <math.h>
 
 #include <alsa/asoundlib.h>
+/*
+ * example:     https://elinux.org/RPi_GPIO_Code_Samples#bcm2835_library
+ * download:    airspayce.com/mikem/bcm2835/
+ */
+#include <bcm2835.h>
 
 #define DEBUG_ENABLE (1)
 #include "include/ampiseq.h"
+
 
 int main(void) {
     return 0;
