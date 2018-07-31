ee25f02ed7cfca3a63b05af65dcc7530b5b20401
SemjonKerner <pi@rpikw.localdomain>
2018-07-31 13:33:20 +0200

    init gpio test

--- ampis/main.py
+++ ampis/main.py
@@ -1,27 +1,32 @@
 # simple gpio tutorial program with python
-# https://tutorials-raspberrypi.de/raspberry-pi-gpio-erklaerung-beginner-programmierung-lernen/
 #
-# pip install RPi.GPIO
+# sudo pip3 install RPi.GPIO
 #
-# use a switch between gpio 24 and 3.3v with pulldown resistor 10kOhm
-# use an LED + R:330Ohm between GND and gpio 23
+# use a switch between gpio 10 and 3.3v with pulldown resistor 220 Ohm
+# use an LED + R:330 Ohm between GND and gpio 23
 
 import RPi.GPIO as GPIO
-import time
 
-GPIO.setmode(GPIO.BCM)
+state = 0
 
-GPIO.setup(23, GPIO.OUT)
-GPIO.setup(24, GPIO.IN)
-
-for i in range(5):
-    GPIO.output(23, GPIO.HIGH)
-    time.sleep(0.5)
-    GPIO.output(23, GPIO.LOW)
-    time.sleep(0.5)
-
-while True:
-    if GPIO.input(24) == 0:
-        GPIO.output(23, GPIO.LOW)
+def button_callback(channel):
+    global state
+    if state == 0:
+        GPIO.output(11, GPIO.HIGH)
+        state = 1
     else:
-        GPIO.output(23, GPIO.HIGH)
+        GPIO.output(11, GPIO.LOW)
+        state = 0
+    print("Button was pushed!")
+
+GPIO.setwarnings(False)
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT)
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+GPIO.add_event_detect(10, GPIO.FALLING, callback=button_callback)
+
+message = input("Press enter to quit\n\n")
+GPIO.output(11, GPIO.LOW)
+GPIO.cleanup()
